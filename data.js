736791bc33f77e6e135dd3dbd48c16444d6b29e4
arulzzzxd <arulzz.xd@gmail.com>
2026-06-06 23:02:18 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+const data = [
+    {
+    "id": 1780759177113,
+    "title": "Contoh",
+    "language": "javascript",
+    "desc": "By arulzxd",
+    "date": "6 Jun 2026",
+    "content": "Contoh"
+}
+];
