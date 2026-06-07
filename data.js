bd31d4590be37f6e70e40aabc2b0c5109d38f92e
arulzzzxd <arulzz.xd@gmail.com>
2026-06-07 12:16:30 +0700

    Update data.js

--- data.js
+++ data.js
@@ -14,5 +14,85 @@ const data = [
     "desc": "By arulzxd",
     "date": "7 Jun 2026",
     "content": "Contoh"
+},
+{
+    "id": 1780809298736,
+    "title": "Contoh2",
+    "language": "javascript",
+    "desc": "By arulzxd",
+    "date": "7 Jun 2026",
+    "content": "Contoh3"
+},
+{
+    "id": 1780809296781,
+    "title": "Contoh2",
+    "language": "javascript",
+    "desc": "By arulzxd",
+    "date": "7 Jun 2026",
+    "content": "Contoh3"
+},
+{
+    "id": 1780809294701,
+    "title": "Contoh2",
+    "language": "javascript",
+    "desc": "By arulzxd",
+    "date": "7 Jun 2026",
+    "content": "Contoh3"
+},
+{
+    "id": 1780809292393,
+    "title": "Contoh2",
+    "language": "javascript",
+    "desc": "By arulzxd",
+    "date": "7 Jun 2026",
+    "content": "Contoh3"
+},
+{
+    "id": 1780809290518,
+    "title": "Contoh2",
+    "language": "javascript",
+    "desc": "By arulzxd",
+    "date": "7 Jun 2026",
+    "content": "Contoh3"
+},
+{
+    "id": 1780809288733,
+    "title": "Contoh2",
+    "language": "javascript",
+    "desc": "By arulzxd",
+    "date": "7 Jun 2026",
+    "content": "Contoh3"
+},
+{
+    "id": 1780809286849,
+    "title": "Contoh2",
+    "language": "javascript",
+    "desc": "By arulzxd",
+    "date": "7 Jun 2026",
+    "content": "Contoh3"
+},
+{
+    "id": 1780809284931,
+    "title": "Contoh2",
+    "language": "javascript",
+    "desc": "By arulzxd",
+    "date": "7 Jun 2026",
+    "content": "Contoh3"
+},
+{
+    "id": 1780809281783,
+    "title": "Contoh2",
+    "language": "javascript",
+    "desc": "By arulzxd",
+    "date": "7 Jun 2026",
+    "content": "Contoh3"
+},
+{
+    "id": 1780809278170,
+    "title": "Contoh2",
+    "language": "javascript",
+    "desc": "By arulzxd",
+    "date": "7 Jun 2026",
+    "content": "Contoh3"
 }
 ];
